eeefe6cfc4497e6e12a923d36e126be858ba20c9
Marcin <ziptofaf1@gmail.com>
2014-07-31 17:08:16 +0200

    modified server api so it handles more exceptions and adds things to logs

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -18,12 +18,16 @@ respond_to :json
   @response = {'status'=>'failure'} and return unless api_params[:login] && api_params[:password] && api_params[:api_key]=='0mfd1INmx86TAzY3U25O' && api_params[:server_id]
   account = Account.find_by login: api_params[:login]
   server = Server.find(api_params[:server_id])
-  @response = {'status'=>'failure'} and return unless account && server && account.server_pool_id==server.server_pool_id && account.expire>Time.now && account.password==api_params[:password] && account.active<=1
+  @response = {'status'=>'failure'} and return unless account && server && account.server_pool_id>=server.server_pool_id && account.expire>Time.now && account.password==api_params[:password] && account.active<=1
   @response = {'status'=>'success'}
-  user.active+=1
-  user.save
+  begin
+  account.active+=1
+  account.save!
   server.capacity_current+=1
-  server.save
+  server.save!
+  rescue => e
+	logger.fatal "#{e}"
+  end
   end
 
   def check_ip
@@ -39,16 +43,16 @@ respond_to :json
   end
 
  def disconnect
-  @response = {'status'=>'failure'} and return unless logs_params[:username] && logs_params[:kilobytes_sent] && logs_params[:kilobytes_received] &&
-  logs_params[:server_id] && logs_params[:api_key] && logs_params[:session_length]
-  @response = {'status'=>'failure'} and return unless logs_params[:api_key]=='0mfd1INmx86TAzY3U25O'
-  @response = {'status'=>'failure'} and logger.fatal "VPN with that username (#{logs_params[:username]}) SHOULDN'T EXIST. Security breach?" and return unless Account.exists?(:login => logs_params[:username])
+  @response = {'status'=>'disconnection failure'} and return unless logs_params[:username] && logs_params[:kilobytes_sent] && logs_params[:kilobytes_received] && logs_params[:server_id] && logs_params[:api_key] && logs_params[:session_length]
+  @response = {'status'=>'disconnection failure'} and return unless logs_params[:api_key]=='0mfd1INmx86TAzY3U25O'
+  @response = {'status'=>'disconnection failure'} and logger.fatal "VPN with that username (#{logs_params[:username]}) SHOULDN'T EXIST. Security breach?" and return unless Account.exists?(:login => logs_params[:username])
+   begin
   account = Account.find_by login: logs_params[:username]
   account.active = account.active - 1
-  account.save
+  account.save!
   server = Server.find(logs_params[:server_id])
   server.capacity_current-=1
-  server.save
+  server.save!
   log = AccountLog.new
   log.account_id=account.id
   log.kilobytes_sent=logs_params[:kilobytes_sent].to_i
@@ -56,8 +60,12 @@ respond_to :json
   log.remote = logs_params[:remote_ip]
   log.end = Time.now
   log.start = Time.now - logs_params[:session_length].to_i
-  log.save
-  @response = {'status'=>'success'}
+  log.save!
+  @response = {'status'=>'disconnection successful'}
+  rescue => e
+   logger.fatal "#{e}"
+   @response = {'status'=>'disconnection failure'}
+  end
 #TODO: MAKE IT SHOVE ERROR MESSAGES UP YOUR ASS SO SERVER CAN RESEND THIS DATA (AKA EXCEPTIONS)
  end
 end
